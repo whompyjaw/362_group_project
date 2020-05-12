7176fbc85c1f1f25df9270b9164369a71c5f96f2
Glenn Oberlander <25990657+whompyjaw@users.noreply.github.com>
2020-05-11 19:51:51 -0700

    Update tests.py

--- tests.py
+++ tests.py
@@ -9,7 +9,7 @@ class TestCase(unittest.TestCase):
 
     def test2(self):
         expected = "Goodbye World"
-        self.assertEqual(task.my_func(), expected)
+        self.assertNotEqual(task.my_func(), expected)
 
 
 if __name__ == '__main__':
